2a76e5bef5ac8b9e864803e806a1f470edb7771b
Banegas, Nicolás <banegasnico@gmail.com>
2025-11-13 13:08:25 -0300

    error ViajeTest solucionado

--- test/viaje/ViajeTest.java
+++ test/viaje/ViajeTest.java
@@ -67,13 +67,12 @@ public class ViajeTest {
 
 		when(tramo1.getOrigen()).thenReturn(t1);
 		when(tramo1.getDestino()).thenReturn(t2);
-		when(tramo1.getDuracion()).thenReturn(Duration.ofDays(2));
 
 		when(tramo2.getOrigen()).thenReturn(t2);
 		when(tramo2.getDestino()).thenReturn(t3);
-		when(tramo2.getDuracion()).thenReturn(Duration.ofDays(3));
 
 		when(circuito.getTramos()).thenReturn(List.of(tramo1, tramo2));
+		when(circuito.getTodasLasTerminales()).thenReturn(List.of(t1, t2, t3));
 
 		Map<ITerminal, LocalDateTime> cronograma = viaje.cronograma();
 
@@ -92,6 +91,15 @@ public class ViajeTest {
 
 	@Test
 	void rutaMaritimaDesdeHasta_DeberiaCrearUnaRutaMaritimaCorrecta() {
+		when(tramo1.getOrigen()).thenReturn(t1);
+		when(tramo1.getDestino()).thenReturn(t2);
+
+		when(tramo2.getOrigen()).thenReturn(t2);
+		when(tramo2.getDestino()).thenReturn(t3);
+
+		when(circuito.getTramos()).thenReturn(List.of(tramo1, tramo2));
+		when(circuito.getTodasLasTerminales()).thenReturn(List.of(t1, t2, t3));
+		
 		IRutaMaritima ruta = viaje.rutaMaritimaDesde_Hasta_(t1, t2);
 
 		assertNotNull(ruta);
